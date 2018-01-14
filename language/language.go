4926dddadc9662b9be83a6197fb33534fa61e111
algon-320 <algon.0320@gmail.com>
2018-01-15 05:01:30 +0900

    言語定義の仕組みを変更

--- language/language.go
+++ language/language.go
@@ -1,10 +1,13 @@
 package language
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/algon-320/KIDE/util"
 )
@@ -23,6 +26,108 @@ type Language interface {
 	UnComment(line string) string
 }
 
+// LanguageBase ... 言語定義用(LanguageインターフェースのRunメソッド以外を実装済み)
+type LanguageBase struct {
+	name           string
+	fileExtension  string
+	compileCommand string // {SOURCEFILE_PATH} の部分がすべてソースコードのパスに置換される
+	runningCommand string // {SOURCEFILE_PATH} の部分がすべてソースコードのパスに置換される
+	commentBegin   string
+	commentEnd     string
+}
+
+// Name ... 言語の名前を返す
+func (l *LanguageBase) Name() string {
+	return l.name
+}
+func (l *LanguageBase) String() string {
+	return l.Name()
+}
+
+// FileExtension ... ソースファイルの拡張子
+func (l *LanguageBase) FileExtension() string {
+	return l.fileExtension
+}
+
+// CommentOut ... line で与えられた文字列をコメントアウトして返す
+func (l *LanguageBase) CommentOut(line string) string {
+	return l.commentBegin + line + l.commentEnd
+}
+
+// UnComment ... commentedLine で与えられたコメントアウトされた文字列のコメントを外して返す
+func (l *LanguageBase) UnComment(commentedLine string) string {
+	lenBegin := len(l.commentBegin)
+	lenEnd := len(l.commentEnd)
+	return commentedLine[lenBegin : len(commentedLine)-lenEnd]
+}
+
+// compile ... sourcePath で与えられたパスのソースコードをコンパイルする(変更がない場合は何もしない)
+func (l *LanguageBase) compile(sourcePath string) error {
+	skip, err := checkSkipCompile(sourcePath) // 変更があるか確認
+	if err != nil {
+		return err
+	}
+	if skip {
+		util.DebugPrint("Source file isn't changed. Skip compiling.")
+		return nil
+	}
+
+	util.DebugPrint("Source file is changed. Compiling ...")
+
+	cmd := util.Command(strings.Replace(l.compileCommand, "{SOURCEFILE_PATH}", sourcePath, -1))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return &ErrCompileError{}
+	}
+	util.DebugPrint("Successfully compiled!")
+	return copySourceFile(sourcePath)
+}
+
+// Run ... 実行
+// input : 標準入力として与える文字列
+// print : 標準出力、標準エラー出力を画面に出力するかどうか
+// return : 実行結果の標準出力, この関数のエラー
+func (l *LanguageBase) Run(sourcePath string, input string, print bool) (string, error) {
+	if l.compileCommand != "" {
+		if err := l.compile(sourcePath); err != nil {
+			return "", err
+		}
+	}
+
+	ret := new(bytes.Buffer)
+	cmd := util.Command(strings.Replace(l.runningCommand, "{SOURCEFILE_PATH}", sourcePath, -1))
+
+	var stdin io.Reader
+	var stdout io.Writer
+	var stderr io.Writer
+	if input == "" {
+		stdin = os.Stdin
+	} else {
+		stdin = bytes.NewBufferString(input)
+	}
+
+	if print {
+		stdout = io.MultiWriter(os.Stdout, ret)
+		stderr = os.Stderr
+	} else {
+		stdout = ret
+		stderr = nil
+	}
+
+	cmd.Stdin = stdin
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		return "", &ErrRuntimeError{}
+	}
+
+	return string(ret.Bytes()), nil
+}
+
 // GetLanguage ... 言語名からLanguageを返す(仮) TODO
 func GetLanguage(name string) Language {
 	switch name {
@@ -38,7 +143,7 @@ func GetLanguage(name string) Language {
 	}
 }
 
-// FindSourceCode ... ソースコードのファイル名を返す
+// FindSourceCode ... 実行する対象のソースコードを決めて、ファイル名を返す(複数ある場合はユーザに問う)
 func FindSourceCode(lang Language) (string, error) {
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
@@ -69,8 +174,7 @@ func FindSourceCode(lang Language) (string, error) {
 	}
 }
 
-//--------------------------------------------------------------------
-
+// utility ---------------------------------------------------------------------
 func checkSkipCompile(sourcePath string) (bool, error) {
 	sourcePathAbs, _ := filepath.Abs(sourcePath)
 	edir, _ := os.Executable()
@@ -92,7 +196,6 @@ func checkSkipCompile(sourcePath string) (bool, error) {
 	}
 	return false, nil
 }
-
 func copySourceFile(sourcePath string) error {
 	p, _ := os.Executable()
 	dir := filepath.Dir(p)
--- language/python.go
+++ language/python.go
@@ -1,82 +1,15 @@
 package language
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-	"os"
-	"strings"
-
-	"github.com/algon-320/KIDE/util"
-)
-
 // PYTHON ... Python2
-var PYTHON = &python{
-	name:          "Python2",
-	fileExtension: ".py",
-	runCommand:    "python {SOURCEFILE_PATH}",
+var PYTHON = &LanguageBase{
+	name:           "Python2",
+	fileExtension:  ".py",
+	compileCommand: "",
+	runningCommand: "python {SOURCEFILE_PATH}",
+	commentBegin:   "# ",
+	commentEnd:     "",
 }
 
 type python struct {
-	name          string
-	fileExtension string
-	runCommand    string // `{SOURCEFILE_PATH}`の部分がソースファイルのパスに置き換わる
-}
-
-func (l *python) Name() string {
-	return l.name
-}
-
-func (l *python) FileExtension() string {
-	return l.fileExtension
-}
-
-// Run ... 実行
-// input : 標準入力として与える文字列
-// print : 標準出力、標準エラー出力を画面に出力するかどうか
-// return : 実行結果の標準出力, エラー
-func (l *python) Run(sourcePath string, input string, print bool) (string, error) {
-	ret := new(bytes.Buffer)
-	l.runCommand = strings.Replace(l.runCommand, "{SOURCEFILE_PATH}", sourcePath, -1)
-	cmd := util.Command(l.runCommand)
-
-	var stdin io.Reader
-	var stdout io.Writer
-	var stderr io.Writer
-	if input == "" {
-		stdin = os.Stdin
-	} else {
-		stdin = bytes.NewBufferString(input)
-	}
-
-	if print {
-		stdout = io.MultiWriter(os.Stdout, ret)
-		stderr = os.Stderr
-	} else {
-		stdout = ret
-		stderr = nil
-	}
-
-	cmd.Stdin = stdin
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println(err)
-		return "", &ErrRuntimeError{}
-	}
-
-	return string(ret.Bytes()), nil
-}
-
-func (l *python) CommentOut(text string) string {
-	return "# " + text
-}
-func (l *python) UnComment(comment string) string {
-	text := comment[2:]
-	return text
-}
-
-func (l *python) String() string {
-	return l.name
+	LanguageBase
 }
